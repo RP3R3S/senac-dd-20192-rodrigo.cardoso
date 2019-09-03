8239286d286db0b3a8e59b9a21ce6d28b0afb8f8
Rodrigo Peres Cardoso <rperes@outlook.com.br>
2019-09-03 19:25:38 -0300

    TelefoneController

---
+++ src/main/java/controller/aula05/TelefoneController.java
@@ -0,0 +1,24 @@
+package controller.aula05;
+
+import java.util.ArrayList;
+
+import model.bo.aula05.TelefoneBO;
+import model.entity.aula05.Telefone;
+
+public class TelefoneController {
+
+	// Chama a lista de telefones assim que "Adicionar Telefone" for verdadeiro.
+	public static final String TIPO_TELEFONE_MOVEL = "Móvel";
+	public static final String TIPO_TELEFONE_FIXO = "Fixo";
+
+	private TelefoneBO bo = new TelefoneBO();
+
+	public ArrayList<Telefone> consultarTodos() {
+		return bo.consultarTodos();
+	}
+
+	public ArrayList<Telefone> consultarTelefonesDoCliente(int idCliente) {
+		return bo.consultarTelefonesDoCliente(idCliente);
+	}
+
+}
